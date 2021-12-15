85c47e0cefe40e038fbcd93ec360f92e5ec6efb2
Kevin F <Freymaurer@gmx.de>
2021-12-15 17:10:04 +0100

    Update installer (Issue #181).

--- .assets/assets/install.cmd
+++ .assets/assets/install.cmd
@@ -1,15 +1,23 @@
 @ECHO OFF
 
+@REM https://docs.microsoft.com/en-us/office/dev/add-ins/testing/create-a-network-shared-folder-catalog-for-task-pane-and-content-add-ins
+
+SET nfdiFolderName=nfdi_manifests
+SET nfdiFolder=%LOCALAPPDATA%\%nfdiFolderName%
+SET computer=%computername%
+SET user=%USERNAME%
+SET nfdiShareName=nfdi_manifest_%user%
+
 REM BatchGotAdmin; https://stackoverflow.com/a/10052222/12858021
 :-------------------------------------
-REM  --> Check for permissions
+@REM  --> Check for permissions
     IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
 >nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
 ) ELSE (
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
 )
 
-REM --> If error flag set, we do not have admin.
+@REM --> If error flag set, we do not have admin.
 if '%errorlevel%' NEQ '0' (
     echo Requesting administrative privileges...
     goto UACPrompt
@@ -29,30 +37,25 @@ if '%errorlevel%' NEQ '0' (
     CD /D "%~dp0"
 :--------------------------------------   
 
-@REM https://docs.microsoft.com/en-us/office/dev/add-ins/testing/create-a-network-shared-folder-catalog-for-task-pane-and-content-add-ins
-
-SET swateFolderName=nfdi_manifests
-SET swateFolder=%LOCALAPPDATA%\%swateFolderName%
-
 @REM create folder in user folder
-IF EXIST %swateFolder% (
+IF EXIST %nfdiFolder% (
     ECHO Found existing folder for Swate manifests!
 ) ELSE (
-    mkdir %swateFolder%
+    mkdir %nfdiFolder%
 )
 
-ECHO Created folder for Swate manifest @%swateFolder%.
+ECHO Created folder for Swate manifest @%nfdiFolder%.
 
 ECHO Download Swate manifest to folder..
 
 @REM Download Swate manifests into new folder
-curl.exe --output %swateFolder%/swateCore_manifest.xml --url https://raw.githubusercontent.com/nfdi4plants/Swate/developer/.assets/assets/core_manifest.xml
-curl.exe --output %swateFolder%/swateExperts_manifest.xml --url https://raw.githubusercontent.com/nfdi4plants/Swate/developer/.assets/assets/experts_manifest.xml
+curl.exe --output %nfdiFolder%/swateCore_manifest.xml --url https://raw.githubusercontent.com/nfdi4plants/Swate/developer/.assets/assets/core_manifest.xml
+curl.exe --output %nfdiFolder%/swateExperts_manifest.xml --url https://raw.githubusercontent.com/nfdi4plants/Swate/developer/.assets/assets/experts_manifest.xml
 
 ECHO Share folder with Excel network..
 
 @REM Share folder with user
-net share swate_manifests=%swateFolder% /grant:%USERNAME%,FULL
+net share %nfdiShareName%=%nfdiFolder% /grant:%user%,FULL
 
 ECHO Create registry file for Excel..
 
@@ -62,13 +65,13 @@ ECHO Create registry file for Excel..
     ECHO ""
     ECHO [HKEY_CURRENT_USER\Software\Microsoft\Office\16.0\WEF\TrustedCatalogs\{aa2b5cf8-00a8-4d2e-9781-8c00ac234d73}]
     ECHO "Id"="{aa2b5cf8-00a8-4d2e-9781-8c00ac234d73}"
-    ECHO "Url"="\\\\%computername%\\%swateFolderName%"
+    ECHO "Url"="\\\\%computer%\\%nfdiShareName%"
     ECHO "Flags"=dword:00000001 
-) > %swateFolder%/TrustNetworkShareCatalog.reg
+) > %nfdiFolder%/TrustNetworkShareCatalog.reg
 
 ECHO Execute registry file for Excel..
 
-%swateFolder%/TrustNetworkShareCatalog.reg
+%nfdiFolder%/TrustNetworkShareCatalog.reg
 
 REM https://stackoverflow.com/questions/2048509/how-to-echo-with-different-colors-in-the-windows-command-line
 
