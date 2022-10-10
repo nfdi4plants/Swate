30784e07bedb1eda76bd2a56b294e6ca5e18796c
Kevin F <Freymaurer@gmx.de>
2022-10-10 16:24:29 +0200

    Update RELEASE_NOTES.md

--- src/Server/Version.fs
+++ src/Server/Version.fs
@@ -3,11 +3,11 @@ namespace System
 open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("SWATE")>]
-[<assembly: AssemblyVersionAttribute("0.6.1")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","22.09.2022")>]
+[<assembly: AssemblyVersionAttribute("0.6.2")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","10.10.2022")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "SWATE"
-    let [<Literal>] AssemblyVersion = "0.6.1"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "22.09.2022"
+    let [<Literal>] AssemblyVersion = "0.6.2"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "10.10.2022"
