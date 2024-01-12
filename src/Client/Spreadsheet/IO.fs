94984720ea1551230487181e2abb167b4218e7f8
Kevin F <Freymaurer@gmx.de>
2024-01-12 13:01:18 +0100

    Allow read in from template xlsx

--- src/Client/Spreadsheet/IO.fs
+++ src/Client/Spreadsheet/IO.fs
@@ -4,20 +4,47 @@ open ARCtrl.ISA
 open ARCtrl.ISA.Spreadsheet
 open FsSpreadsheet.Exceljs
 open Shared
+open FsSpreadsheet
+
+let private tryToConvertAssay (fswb: FsWorkbook) =
+    try
+        ArcAssay.fromFsWorkbook fswb |> Assay |> Some
+    with
+        | _ -> None
+
+let private tryToConvertStudy (fswb: FsWorkbook) =
+    try
+        ArcStudy.fromFsWorkbook fswb |> Study |> Some
+    with
+        | _ -> None
+
+let private tryToConvertInvestigation (fswb: FsWorkbook) =
+    try
+        ArcInvestigation.fromFsWorkbook fswb |> Investigation |> Some
+    with
+        | _ -> None
+
+let private tryToConvertTemplate (fswb: FsWorkbook) =
+    try
+        ARCtrl.Template.Spreadsheet.Template.fromFsWorkbook fswb |> Template |> Some
+    with
+        | _ -> None
+
+// List of conversion functions
+let private converters = [tryToConvertAssay; tryToConvertStudy; tryToConvertInvestigation; tryToConvertTemplate]
 
 // TODO: Can this be done better? If we want to allow upload of any isa.xlsx file?
 let readFromBytes (bytes: byte []) =
+    // Try each conversion function and return the first successful result
+    let rec tryConvert (converters: ('a -> 'b option) list) (json: 'a) =
+        match converters with
+        | [] -> None
+        | convert :: rest ->
+            match convert json with
+            | Some result -> Some result
+            | None -> tryConvert rest json
     promise {
         let! fswb = FsSpreadsheet.Exceljs.Xlsx.fromBytes bytes
-        let r =
-            try
-                ArcAssay.fromFsWorkbook fswb |> Assay
-            with
-                | _ ->
-                    try 
-                        ArcStudy.fromFsWorkbook fswb |> Study
-                    with
-                        | _ ->
-                            ArcInvestigation.fromFsWorkbook fswb |> Investigation
-        return r
+        let arcFile = tryConvert converters fswb
+        return arcFile
     }
