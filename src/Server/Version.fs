755e07fb4216ce48d3eeafe5e9fbe55f3b45bed4
Kevin F <Freymaurer@gmx.de>
2024-01-31 17:41:23 +0100

    Release vv1.0.0-alpha.06 :bookmark:

--- src/Server/Version.fs
+++ src/Server/Version.fs
@@ -4,12 +4,12 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Swate")>]
 [<assembly: AssemblyVersionAttribute("1.0.0")>]
-[<assembly: AssemblyMetadataAttribute("Version","v1.0.0-alpha.05")>]
+[<assembly: AssemblyMetadataAttribute("Version","v1.0.0-alpha.06")>]
 [<assembly: AssemblyMetadataAttribute("ReleaseDate","31.01.2024")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Swate"
     let [<Literal>] AssemblyVersion = "1.0.0"
-    let [<Literal>] AssemblyMetadata_Version = "v1.0.0-alpha.05"
+    let [<Literal>] AssemblyMetadata_Version = "v1.0.0-alpha.06"
     let [<Literal>] AssemblyMetadata_ReleaseDate = "31.01.2024"
