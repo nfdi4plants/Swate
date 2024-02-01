a0b0ad1ac60ea99960884a55a609987a36fcae46
Kevin F <Freymaurer@gmx.de>
2024-02-01 11:02:45 +0100

    Improve search cell robustness :bug:#348

--- src/Client/SharedComponents/ClickOutsideHandler.fs
+++ src/Client/SharedComponents/ClickOutsideHandler.fs
@@ -9,18 +9,26 @@ type ClickOutsideHandler =
 
     static member AddListener(containerId: string, clickedOutsideEvent: Event -> unit) =
         let rec closeEvent = fun (e: Event) ->
+            let rmv = fun () -> Browser.Dom.document.removeEventListener("click", closeEvent)
             let dropdown = Browser.Dom.document.getElementById(containerId)
-            let isClickedInsideDropdown : bool = dropdown?contains(e.target)
-            if not isClickedInsideDropdown then 
-                clickedOutsideEvent e
-                Browser.Dom.document.removeEventListener("click", closeEvent)
+            if isNull dropdown then 
+                rmv()
+            else
+                let isClickedInsideDropdown : bool = dropdown?contains(e.target)
+                if not isClickedInsideDropdown then 
+                    clickedOutsideEvent e
+                    rmv()
         Browser.Dom.document.addEventListener("click", closeEvent)
 
     static member AddListener(element: IRefValue<HTMLElement option>, clickedOutsideEvent: Event -> unit) =
         let rec closeEvent = fun (e: Event) ->
+            let rmv = fun () -> Browser.Dom.document.removeEventListener("click", closeEvent)
             let dropdown = element.current
-            let isClickedInsideDropdown : bool = dropdown?contains(e.target)
-            if not isClickedInsideDropdown then 
-                clickedOutsideEvent e
-                Browser.Dom.document.removeEventListener("click", closeEvent)
+            if dropdown.IsNone then
+                rmv()
+            else 
+                let isClickedInsideDropdown : bool = dropdown?contains(e.target)
+                if not isClickedInsideDropdown then 
+                    clickedOutsideEvent e
+                    rmv()
         Browser.Dom.document.addEventListener("click", closeEvent)
