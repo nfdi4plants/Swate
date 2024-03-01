a41cb31cfee8dd0eb2117279e4a60cf87b890ff1
Kevin F <Freymaurer@gmx.de>
2024-03-01 16:28:55 +0100

    Add row number #342

--- src/Client/MainComponents/SpreadsheetView.fs
+++ src/Client/MainComponents/SpreadsheetView.fs
@@ -9,18 +9,6 @@ open Spreadsheet.Cells
 open ARCtrl.ISA
 open Shared
 
-
-let private referenceColumns (columnIndex, header: CompositeHeader, state: Set<int>, setState, model, dispatch) =
-    if header.IsTermColumn then
-        [
-            let isExtended = state.Contains columnIndex
-            if isExtended then
-                Cell.HeaderUnit(columnIndex, header, state, setState, model, dispatch)
-                Cell.HeaderTSR(columnIndex, header, state, setState, model, dispatch)
-                Cell.HeaderTAN(columnIndex, header, state, setState, model, dispatch)
-        ]
-    else []
-
 let cellPlaceholder (c_opt: CompositeCell option) =
     let tableCell (children: ReactElement list) = Html.td [
         Html.div [
@@ -40,9 +28,29 @@ let cellPlaceholder (c_opt: CompositeCell option) =
             ]
     ]
 
+/// <summary>
+/// rowIndex < 0 equals header
+/// </summary>
+/// <param name="rowIndex"></param>
+let RowLabel (rowIndex: int) = 
+    let t : IReactProperty list -> ReactElement = if rowIndex < 0 then Html.th else Html.td 
+    t [
+        prop.style [style.resize.none; style.border(length.px 1, borderStyle.solid, "darkgrey")]
+        prop.children [
+            Bulma.button.button [
+                prop.className "px-2 py-1"
+                prop.style [style.custom ("border", "unset"); style.borderRadius 0]
+                Bulma.button.isFullWidth
+                Bulma.button.isStatic
+                prop.text (if rowIndex < 0 then "" else $"{rowIndex+1}")
+            ]
+        ]
+    ]
+
 let private bodyRow (rowIndex: int) (state:Set<int>) setState (model:Model) (dispatch: Msg -> unit) =
     let table = model.SpreadsheetModel.ActiveTable
     Html.tr [
+        RowLabel rowIndex
         for columnIndex in 0 .. (table.ColumnCount-1) do
             let index = columnIndex, rowIndex
             let cell = model.SpreadsheetModel.ActiveTable.Values.[index]
@@ -67,6 +75,7 @@ let private bodyRows (state:Set<int>) setState (model:Model) (dispatch: Msg -> u
 let private headerRow (state:Set<int>) setState (model:Model) (dispatch: Msg -> unit) =
     let table = model.SpreadsheetModel.ActiveTable
     Html.tr [
+        RowLabel -1
         for columnIndex in 0 .. (table.ColumnCount-1) do
             let header = table.Headers.[columnIndex]
             Cells.Cell.Header(columnIndex, header, state, setState, model, dispatch)
