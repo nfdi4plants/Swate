b9be21a74aee40af8c8a6191ce32634bda0fc2b2
HLWeil <hl.weil@t-online.de>
2024-08-21 23:45:30 +0200

    Release v1.0.0-beta.08 :bookmark:

--- src/Server/Version.fs
+++ src/Server/Version.fs
@@ -4,12 +4,12 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Swate")>]
 [<assembly: AssemblyVersionAttribute("1.0.0")>]
-[<assembly: AssemblyMetadataAttribute("Version","v1.0.0-beta.06")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","26.07.2024")>]
+[<assembly: AssemblyMetadataAttribute("Version","v1.0.0-beta.08")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","8/21/2024")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Swate"
     let [<Literal>] AssemblyVersion = "1.0.0"
-    let [<Literal>] AssemblyMetadata_Version = "v1.0.0-beta.06"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "26.07.2024"
+    let [<Literal>] AssemblyMetadata_Version = "v1.0.0-beta.08"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "8/21/2024"
