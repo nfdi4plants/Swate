61a736454dfc6f18eb51b9cf304c31cf37b3b041
Kevin F <Freymaurer@gmx.de>
2024-11-19 16:25:17 +0100

    update to onfocus event :sparkles:

--- src/Client/Pages/TermSearch/TermSearchView.fs
+++ src/Client/Pages/TermSearch/TermSearchView.fs
@@ -83,14 +83,25 @@ let private addButton (model: Model, dispatch) =
 [<ReactComponent>]
 let Main (model:Model, dispatch) =
     let setTerm = fun (term: OntologyAnnotation option) -> TermSearch.UpdateSelectedTerm term |> TermSearchMsg |> dispatch
-    let setParentTerm = fun (parent: OntologyAnnotation option) -> TermSearch.UpdateParentTerm parent |> TermSearchMsg |> dispatch
+
+    let excelGetParentTerm =
+        match model.PersistentStorageState.Host with
+        | Some Swatehost.Excel ->
+            fun _ ->
+                promise {
+                    let! parent = OfficeInterop.Core.Main.getParentTerm()
+                    TermSearch.UpdateParentTerm parent |> TermSearchMsg |> dispatch
+                }
+            |> Some
+        | _ -> None
+
     SidebarComponents.SidebarLayout.Container  [
         SidebarComponents.SidebarLayout.Header "Ontology term search"
 
         SidebarComponents.SidebarLayout.Description "Search for an ontology term to fill into the selected field(s)"
 
         SidebarComponents.SidebarLayout.LogicContainer [
-            Components.TermSearch.Input(setTerm, fullwidth=true, ?parent=model.TermSearchState.ParentTerm, advancedSearchDispatch=dispatch, setParentTerm=setParentTerm)
+            Components.TermSearch.Input(setTerm, fullwidth=true, ?parent=model.TermSearchState.ParentTerm, advancedSearchDispatch=dispatch, ?onFocus=excelGetParentTerm, autofocus=true)
             addButton(model, dispatch)
         ]
     ]
