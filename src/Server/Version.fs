aa684dac74a4c7ea9a0973d1fa4ce83dddf3fada
patrick blume <PatrickBlume@gmx.de>
2025-01-29 13:55:52 +0100

    Release v1.0.0-v1.0.0-beta.20 :bookmark:

--- src/Server/Version.fs
+++ src/Server/Version.fs
@@ -4,12 +4,12 @@ open System.Reflection
 
 [<assembly: AssemblyTitleAttribute("Swate")>]
 [<assembly: AssemblyVersionAttribute("1.0.0")>]
-[<assembly: AssemblyMetadataAttribute("Version","v1.0.0-beta.19")>]
-[<assembly: AssemblyMetadataAttribute("ReleaseDate","1/10/2025")>]
+[<assembly: AssemblyMetadataAttribute("Version","v1.0.0-v1.0.0-beta.20")>]
+[<assembly: AssemblyMetadataAttribute("ReleaseDate","1/29/2025")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Swate"
     let [<Literal>] AssemblyVersion = "1.0.0"
-    let [<Literal>] AssemblyMetadata_Version = "v1.0.0-beta.19"
-    let [<Literal>] AssemblyMetadata_ReleaseDate = "1/10/2025"
+    let [<Literal>] AssemblyMetadata_Version = "v1.0.0-v1.0.0-beta.20"
+    let [<Literal>] AssemblyMetadata_ReleaseDate = "1/29/2025"
