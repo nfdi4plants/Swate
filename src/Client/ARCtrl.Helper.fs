f14760034d5fbf500a3db0a7712aa8962712c2c3
Kevin F <Freymaurer@gmx.de>
2025-01-31 10:23:26 +0100

    Create OntologyAnnotation and Term conversion method

--- src/Client/ARCtrl.Helper.fs
+++ src/Client/ARCtrl.Helper.fs
@@ -4,6 +4,7 @@ open Shared
 open ARCtrl
 open Database
 open System.Collections.Generic
+open Fable.Core.JsInterop
 
 module TermCollection =
 
@@ -341,8 +342,39 @@ module Extensions =
             | None -> failwithf "Unknown DataFile: %s" str
 
     type OntologyAnnotation with
+
+        static member private DescriptionCommentKey = "description"
+        static member private IsObsoleteCommentKey = "isObsolete"
         static member empty() = OntologyAnnotation.create()
-        static member fromTerm (term:Term) = OntologyAnnotation(term.Name, term.FK_Ontology, term.Accession)
+        static member fromTerm (term:Swate.Components.Term) =
+            let comments = ResizeArray [
+                if term.description.IsSome then
+                    Comment(OntologyAnnotation.DescriptionCommentKey, term.description.Value)
+                if term.isObsolete.IsSome then
+                    Comment(OntologyAnnotation.IsObsoleteCommentKey, term.isObsolete.Value.ToString())
+            ]
+            OntologyAnnotation(?name = term.name, ?tsr = term.source, ?tan = term.id, comments = comments)
+
+        static member fromDBTerm (term:Term) =
+            let comments = ResizeArray [
+                if System.String.IsNullOrWhiteSpace term.Description |> not then
+                    Comment(OntologyAnnotation.DescriptionCommentKey, term.Description)
+                if term.IsObsolete then
+                    Comment(OntologyAnnotation.IsObsoleteCommentKey, term.IsObsolete.ToString())
+            ]
+            OntologyAnnotation(term.Name, term.FK_Ontology, term.Accession, comments)
+        member this.ToTerm() =
+            let href = this.TermAccessionOntobeeUrl |> Option.whereNot System.String.IsNullOrWhiteSpace
+            let description = this.Comments |> Seq.tryFind (fun c -> c.Name = Some OntologyAnnotation.DescriptionCommentKey) |> Option.bind (fun c -> c.Value)
+            let isObsolete = this.Comments |> Seq.tryFind (fun c -> c.Name = Some OntologyAnnotation.IsObsoleteCommentKey) |> Option.bind (fun c -> c.Value) |> Option.map System.Boolean.Parse
+            Swate.Components.Term(
+                ?name = this.Name,
+                ?source = this.TermSourceREF,
+                ?id = this.TermAccessionNumber,
+                ?href=href,
+                ?description=description,
+                ?isObsolete=isObsolete
+            )
 
     type ArcTable with
         member this.SetCellAt(columnIndex: int, rowIndex: int, cell: CompositeCell) =
