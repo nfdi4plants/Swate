85fe28d877a7b6334ecc194be26ce88cd34ce4a0
patrick blume <PatrickBlume@gmx.de>
2025-03-27 10:47:58 +0100

    replace old darkmode logic with theme-change

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -1,5 +1,6 @@
 module Index
 
+open System
 open Elmish.Navigation
 open Elmish.UrlParser
 open Elmish
@@ -13,57 +14,62 @@ let sayHello name = $"Hello {name}"
 open Feliz
 open Feliz.DaisyUI
 
+open Fable.Core.JsInterop
+
 [<ReactComponent>]
 let View (model : Model) (dispatch : Msg -> unit) =
-    let colorstate, setColorstate = React.useState(LocalStorage.Darkmode.State.init())
-    // Make ARCitect always use lighttheme
-    let makeColorSchemeLight = fun _ ->
-        if model.PersistentStorageState.Host.IsSome && model.PersistentStorageState.Host.Value = Swatehost.ARCitect then
-            setColorstate {colorstate with Theme = LocalStorage.Darkmode.DataTheme.Light}
-            LocalStorage.Darkmode.DataTheme.SET LocalStorage.Darkmode.DataTheme.Light
-        else
-            setColorstate (colorstate.Update())
-    React.useEffect(makeColorSchemeLight, [|box model.PersistentStorageState.Host|])
-    let v = {colorstate with SetTheme = setColorstate}
+
+    let themeChangeModule: obj = importDefault "theme-change"
+
+    let themeChange =
+        let themeChange: obj = themeChangeModule?themeChange
+        themeChange :?> (bool -> unit)
+
+    React.useEffectOnce (fun () ->
+        themeChange false // Reattach event listeners manually
+        let storedTheme = Browser.WebStorage.localStorage.getItem("theme")
+        if (String.IsNullOrEmpty storedTheme) then
+            Browser.Dom.document.documentElement.setAttribute("data-theme", "light")
+        None
+    )
+
     React.strictMode [
-        React.contextProvider(LocalStorage.Darkmode.themeContext, v,
-            Html.div [
-                prop.id "ClientView"
-                prop.className "flex w-full overflow-auto h-screen"
-                prop.children [
-                    Modals.ModalProvider.Main(model, dispatch)
-                    match model.PageState.IsHome, model.PersistentStorageState.Host with
-                    | false, _ ->
-                        View.MainPageView.Main(model, dispatch)
-                    | _, Some Swatehost.Excel ->
-                        Html.div [
-                            prop.className "flex flex-col w-full h-full"
-                            prop.children [
+        Html.div [
+            prop.id "ClientView"
+            prop.className "flex w-full overflow-auto h-screen"
+            prop.children [
+                Modals.ModalProvider.Main(model, dispatch)
+                match model.PageState.IsHome, model.PersistentStorageState.Host with
+                | false, _ ->
+                    View.MainPageView.Main(model, dispatch)
+                | _, Some Swatehost.Excel ->
+                    Html.div [
+                        prop.className "flex flex-col w-full h-full"
+                        prop.children [
+                            SidebarView.SidebarView.Main(model, dispatch)
+                        ]
+                    ]
+                | _, _ ->
+                    let isActive = model.SpreadsheetModel.TableViewIsActive() && model.PageState.ShowSideBar
+                    Daisy.drawer [
+                        prop.className [
+                            "drawer-end"
+                            if isActive then "drawer-open"
+                        ]
+                        prop.children [
+                            Html.input [
+                                prop.id "split-window-drawer"
+                                prop.type'.checkbox
+                                prop.className "drawer-toggle"
+                            ]
+                            Daisy.drawerContent [
+                                SpreadsheetView.Main (model, dispatch)
+                            ]
+                            Daisy.drawerSide [
                                 SidebarView.SidebarView.Main(model, dispatch)
                             ]
                         ]
-                    | _, _ ->
-                        let isActive = model.SpreadsheetModel.TableViewIsActive() && model.PageState.ShowSideBar
-                        Daisy.drawer [
-                            prop.className [
-                                "drawer-end"
-                                if isActive then "drawer-open"
-                            ]
-                            prop.children [
-                                Html.input [
-                                    prop.id "split-window-drawer"
-                                    prop.type'.checkbox
-                                    prop.className "drawer-toggle"
-                                ]
-                                Daisy.drawerContent [
-                                    SpreadsheetView.Main (model, dispatch)
-                                ]
-                                Daisy.drawerSide [
-                                    SidebarView.SidebarView.Main(model, dispatch)
-                                ]
-                            ]
-                        ]
-                ]
+                    ]
             ]
-        )
+        ]
     ]
--- src/Client/Pages/Settings/SettingsView.fs
+++ src/Client/Pages/Settings/SettingsView.fs
@@ -11,57 +11,38 @@ open Messages
 
 open Feliz
 open Feliz.DaisyUI
-open LocalStorage.Darkmode
+open Browser.Dom
 
 type Settings =
 
+    static member themeChangeModule: obj = importDefault "theme-change"
+
     [<ReactComponent>]
     static member ThemeToggle () =
-        let darkmodeState = React.useContext(LocalStorage.Darkmode.themeContext)
-        darkmodeState.Update() |> ignore
-        let isDark = darkmodeState.Theme = Dark
-        Html.label [
-            prop.className "grid lg:col-span-2 grid-cols-subgrid cursor-pointer not-prose"
-            prop.onClick (fun e ->
-                e.preventDefault()
-                let next = if isDark then Light else Dark
-                DataTheme.SET next
-                darkmodeState.SetTheme {darkmodeState with Theme = next}
-            )
-            prop.children [
-                Html.p [
-                    prop.className "select-none text-xl py-2"
-                    prop.text "Theme"
-                ]
-                Html.div [
-                    prop.className [
-                        "btn btn-block swap swap-rotate"
-                        if isDark then "swap-active";
-                    ]
-                    prop.children [
-                        Svg.svg [
-                            svg.className "size-6 fill-current swap-off"
-                            svg.xmlns "http://www.w3.org/2000/svg"
-                            svg.viewBox (0, 0, 24, 24)
-                            svg.children [
-                                Svg.path [
-                                    svg.d "M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z"
-                                ]
-                            ]
-                        ]
-                        Svg.svg [
-                            svg.className "size-6 fill-current swap-on"
-                            svg.xmlns "http://www.w3.org/2000/svg"
-                            svg.viewBox (0, 0, 24, 24)
-                            svg.children [
-                                Svg.path [
-                                    svg.d "M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z"
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ]
+
+        let currentTheme, setTheme = React.useState(document.documentElement.getAttribute("data-theme"))
+
+        let themeChange =
+            let themeChange: obj = Settings.themeChangeModule?themeChange
+            themeChange :?> (bool -> unit)
+
+        React.useEffectOnce (fun () ->
+            themeChange false // Reattach event listeners manually
+            None
+        )
+
+        let newTheme, icon =
+            match currentTheme with
+            | "light"   -> "dark", "🌙 Dark Mode"
+            | "dark"    -> "light", "☀️ Light Mode"
+            | _         -> "dark", "🌙 Dark Mode"
+
+        Html.button [
+            prop.className "btn btn-primary"
+            prop.custom ("data-set-theme", newTheme) // Theme-change switches between themes
+            prop.custom ("data-key", "theme")
+            prop.text icon
+            prop.onClick (fun _ -> setTheme newTheme) // Manual override to ensure the change
         ]
 
     static member ToggleAutosaveConfig(model, dispatch) =
