2da9bf9a2f6540ff86eb8abaf97925938f29ed27
Kevin F <Freymaurer@gmx.de>
2025-06-13 14:27:02 +0200

    Add Client versioning

--- src/Client/SidebarComponents/Footer.fs
+++ src/Client/SidebarComponents/Footer.fs
@@ -16,20 +16,28 @@ type Footer =
                 |> PersistentStorageMsg
                 |> dispatch
             }
-            |> Async.StartImmediate)
+            |> Async.StartImmediate
+        )
 
         Html.div [
             prop.className "swt:flex swt:items-center swt:justify-center swt:p-2 swt:text-sm swt:@md/sidebar:text-md"
             prop.children [
                 Html.div [
-                    Html.text "Swate Release Version "
-                    Html.a [
-                        prop.className "swt:link-primary"
-                        prop.href "https://github.com/nfdi4plants/Swate/releases"
-                        prop.target.blank
-                        prop.text model.PersistentStorageState.AppVersion
+                    prop.children [
+                        if
+                            model.PersistentStorageState.AppVersion = System.AssemblyVersionInformation.AssemblyMetadata_Version
+                        then
+                            Html.textf "Version %s " System.AssemblyVersionInformation.AssemblyMetadata_Version
+                        else
+                            Html.textf "Client %s" System.AssemblyVersionInformation.AssemblyMetadata_Version
+                            Html.textf " – Server %s " model.PersistentStorageState.AppVersion
+                        Html.a [
+                            prop.className "swt:link-primary"
+                            prop.href "https://github.com/nfdi4plants/Swate/releases"
+                            prop.target.blank
+                            prop.children [ Html.small "release" ]
+                        ]
                     ]
-                // Html.textf ". Host: %O." model.PersistentStorageState.Host
                 ]
             ]
         ]
