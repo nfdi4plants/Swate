03fc06fd8ce02264838fc7949ccaec4e01a9925a
Kevin F <Freymaurer@gmx.de>
2025-06-13 14:15:11 +0200

    Release  :bookmark:

--- build/Build.fs
+++ build/Build.fs
@@ -55,95 +55,114 @@ module ReleaseNoteTasks =
             Fake.DotNet.AssemblyInfo.Metadata("ReleaseDate", releaseDate)
         ]
 
+        Fake.DotNet.AssemblyInfoFile.createFSharp "src/Client/Version.fs" [
+            Fake.DotNet.AssemblyInfo.Title "Swate"
+            Fake.DotNet.AssemblyInfo.Version version
+            Fake.DotNet.AssemblyInfo.Metadata("Version", version)
+            Fake.DotNet.AssemblyInfo.Metadata("ReleaseDate", releaseDate)
+        ]
+
         if commit then
             run git [ "add"; "." ] ""
             run git [ "commit"; "-m"; (sprintf "Release %s :bookmark:" ProjectInfo.prereleaseTag) ] ""
 
     let updateReleaseNotes =
-        Target.create "releasenotes" (fun config ->
-            ReleaseNotes.ensure ()
+        Target.create
+            "releasenotes"
+            (fun config ->
+                ReleaseNotes.ensure ()
 
-            ReleaseNotes.update (ProjectInfo.gitOwner, ProjectInfo.project, config)
+                ReleaseNotes.update (ProjectInfo.gitOwner, ProjectInfo.project, config)
 
-            let newRelease = ReleaseNotes.load "RELEASE_NOTES.md"
-            createVersionFile (newRelease.AssemblyVersion, false)
+                let newRelease = ReleaseNotes.load "RELEASE_NOTES.md"
+                createVersionFile (newRelease.AssemblyVersion, false)
 
-            Trace.trace "Update Version.fs done!"
+                Trace.trace "Update Version.fs done!"
 
-            // Update maniefest.xmls
-            Trace.trace "Update manifest.xml"
+                // Update maniefest.xmls
+                Trace.trace "Update manifest.xml"
 
-            let _ =
-                let newVer =
-                    sprintf
-                        "<Version>%i.%i.%i</Version>"
-                        newRelease.SemVer.Major
-                        newRelease.SemVer.Minor
-                        newRelease.SemVer.Patch
+                let _ =
+                    let newVer =
+                        sprintf
+                            "<Version>%i.%i.%i</Version>"
+                            newRelease.SemVer.Major
+                            newRelease.SemVer.Minor
+                            newRelease.SemVer.Patch
 
-                Shell.regexReplaceInFilesWithEncoding
-                    "<Version>[0-9]+.[0-9]+.[0-9]+</Version>"
-                    newVer
-                    System.Text.Encoding.UTF8
-                    [
-                        System.IO.Path.Combine(__SOURCE_DIRECTORY__, @"..\.assets\assets\manifest.xml")
-                        System.IO.Path.Combine(__SOURCE_DIRECTORY__, @"..\.assets\assets\core_manifest.xml")
-                        System.IO.Path.Combine(__SOURCE_DIRECTORY__, @"..\.assets\assets\experts_manifest.xml")
-                        System.IO.Path.Combine(__SOURCE_DIRECTORY__, "manifest.xml")
-                    ]
+                    Shell.regexReplaceInFilesWithEncoding
+                        "<Version>[0-9]+.[0-9]+.[0-9]+</Version>"
+                        newVer
+                        System.Text.Encoding.UTF8
+                        [
+                            System.IO.Path.Combine(__SOURCE_DIRECTORY__, @"..\.assets\assets\manifest.xml")
+                            System.IO.Path.Combine(__SOURCE_DIRECTORY__, @"..\.assets\assets\core_manifest.xml")
+                            System.IO.Path.Combine(__SOURCE_DIRECTORY__, @"..\.assets\assets\experts_manifest.xml")
+                            System.IO.Path.Combine(__SOURCE_DIRECTORY__, "manifest.xml")
+                        ]
 
-            Trace.trace "Update manifest.xml done!")
+                Trace.trace "Update manifest.xml done!"
+            )
 
     let githubDraft =
-        Target.create "GithubDraft" (fun config ->
+        Target.create
+            "GithubDraft"
+            (fun config ->
 
-            let zipFolderPath = System.IO.Path.Combine(__SOURCE_DIRECTORY__, @"..\.assets")
+                let zipFolderPath = System.IO.Path.Combine(__SOURCE_DIRECTORY__, @"..\.assets")
 
-            let cleanFolder =
-                Trace.trace $"Clean existing .zip files from '{Path.getFullName (zipFolderPath)}'!"
+                let cleanFolder =
+                    Trace.trace $"Clean existing .zip files from '{Path.getFullName (zipFolderPath)}'!"
 
-                Fake.IO.DirectoryInfo.ofPath zipFolderPath
-                |> Fake.IO.DirectoryInfo.getFiles
-                |> Array.filter (fun x -> x.Name.EndsWith ".zip")
-                |> Array.map (fun x -> x.FullName)
-                |> File.deleteAll
+                    Fake.IO.DirectoryInfo.ofPath zipFolderPath
+                    |> Fake.IO.DirectoryInfo.getFiles
+                    |> Array.filter (fun x -> x.Name.EndsWith ".zip")
+                    |> Array.map (fun x -> x.FullName)
+                    |> File.deleteAll
 
-            let assetPath = System.IO.Path.Combine(zipFolderPath, "assets")
-            let assetDir = Fake.IO.DirectoryInfo.ofPath assetPath
+                let assetPath = System.IO.Path.Combine(zipFolderPath, "assets")
+                let assetDir = Fake.IO.DirectoryInfo.ofPath assetPath
 
-            let allFiles, quickstartFiles =
-                let assetsPaths = Fake.IO.DirectoryInfo.getFiles assetDir
+                let allFiles, quickstartFiles =
+                    let assetsPaths = Fake.IO.DirectoryInfo.getFiles assetDir
 
-                assetsPaths |> Array.map (fun x -> x.FullName),
-                assetsPaths
-                |> Array.filter (fun x -> x.Name = "core_manifest.xml")
-                |> Array.map (fun x -> x.FullName)
+                    assetsPaths |> Array.map (fun x -> x.FullName),
+                    assetsPaths
+                    |> Array.filter (fun x -> x.Name = "core_manifest.xml")
+                    |> Array.map (fun x -> x.FullName)
 
-            let zipFile = System.IO.Path.Combine(zipFolderPath, "swate-win.zip")
+                let zipFile = System.IO.Path.Combine(zipFolderPath, "swate-win.zip")
 
-            let quickStartZipFile =
-                System.IO.Path.Combine(zipFolderPath, "swate-b-quickstart.zip")
+                let quickStartZipFile =
+                    System.IO.Path.Combine(zipFolderPath, "swate-b-quickstart.zip")
 
-            Zip.zip assetDir.FullName zipFile allFiles
+                Zip.zip assetDir.FullName zipFile allFiles
 
-            Zip.zip assetDir.FullName quickStartZipFile (quickstartFiles)
+                Zip.zip assetDir.FullName quickStartZipFile (quickstartFiles)
 
-            Trace.trace "Assets zipped!"
+                Trace.trace "Assets zipped!"
 
-            let bodyText =
-                [
-                    ""
-                    "You can check our [release notes](https://github.com/nfdi4plants/Swate/blob/developer/RELEASE_NOTES.md) to see a list of all new features."
-                    "If you decide to test Swate in the current state, please take the time to set up a Github account to report your issues and suggestions [here](https://github.com/nfdi4plants/Swate/issues/new/choose)."
-                    ""
-                    "You can also search existing issues for solutions for your questions and/or discussions about your suggestions."
-                    ""
-                    "If you want to start using Swate follow these easy instructions: [Swate installation](https://github.com/nfdi4plants/Swate#installuse)"
-                    ""
-                ]
-                |> String.concat "\n"
+                let bodyText =
+                    [
+                        ""
+                        "You can check our [release notes](https://github.com/nfdi4plants/Swate/blob/developer/RELEASE_NOTES.md) to see a list of all new features."
+                        "If you decide to test Swate in the current state, please take the time to set up a Github account to report your issues and suggestions [here](https://github.com/nfdi4plants/Swate/issues/new/choose)."
+                        ""
+                        "You can also search existing issues for solutions for your questions and/or discussions about your suggestions."
+                        ""
+                        "If you want to start using Swate follow these easy instructions: [Swate installation](https://github.com/nfdi4plants/Swate#installuse)"
+                        ""
+                    ]
+                    |> String.concat "\n"
 
-            Github.draft (ProjectInfo.gitOwner, ProjectInfo.project, (Some bodyText), (Some <| zipFolderPath), config))
+                Github.draft (
+                    ProjectInfo.gitOwner,
+                    ProjectInfo.project,
+                    (Some bodyText),
+                    (Some <| zipFolderPath),
+                    config
+                )
+            )
 
 module Docker =
 
@@ -191,9 +210,12 @@ module Docker =
         let dockerComposeNewPath = Path.getFullName ".db/docker.compose.new.yml"
         run dockerCompose [ "-f"; dockerComposeNewPath; "up" ] __SOURCE_DIRECTORY__
 
-    Target.create "docker-test" (fun _ ->
-        dockerCreateImage (Some "new")
-        dockerTestImage (Some "new"))
+    Target.create
+        "docker-test"
+        (fun _ ->
+            dockerCreateImage (Some "new")
+            dockerTestImage (Some "new")
+        )
 
 // Create nightly (https://de.wikipedia.org/wiki/Nightly_Build)
 // 1: docker build -t swate -f build/Dockerfile.publish .
@@ -303,41 +325,52 @@ module Release =
             failwith "aborted"
 
 
-Target.create "InstallOfficeAddinTooling" (fun _ ->
+Target.create
+    "InstallOfficeAddinTooling"
+    (fun _ ->
 
-    printfn "Installing office addin tooling"
+        printfn "Installing office addin tooling"
 
-    run npm [| "install"; "-g"; "office-addin-dev-certs" |] __SOURCE_DIRECTORY__
-    run npm [| "install"; "-g"; "office-addin-debugging" |] __SOURCE_DIRECTORY__
-    run npm [| "install"; "-g"; "office-addin-manifest" |] __SOURCE_DIRECTORY__)
-
-Target.create "SetLoopbackExempt" (fun _ ->
-    Command.RawCommand(
-        "CheckNetIsolation.exe",
-        Arguments.ofList [ "LoopbackExempt"; "-a"; "-n=\"microsoft.win32webviewhost_cw5n1h2txyewy\"" ]
+        run npm [| "install"; "-g"; "office-addin-dev-certs" |] __SOURCE_DIRECTORY__
+        run npm [| "install"; "-g"; "office-addin-debugging" |] __SOURCE_DIRECTORY__
+        run npm [| "install"; "-g"; "office-addin-manifest" |] __SOURCE_DIRECTORY__
     )
-    |> CreateProcess.fromCommand
-    |> Proc.run
-    |> ignore)
 
-Target.create "CreateDevCerts" (fun _ ->
-    run npx [| "office-addin-dev-certs"; "install"; "--days"; "365" |] ""
+Target.create
+    "SetLoopbackExempt"
+    (fun _ ->
+        Command.RawCommand(
+            "CheckNetIsolation.exe",
+            Arguments.ofList [ "LoopbackExempt"; "-a"; "-n=\"microsoft.win32webviewhost_cw5n1h2txyewy\"" ]
+        )
+        |> CreateProcess.fromCommand
+        |> Proc.run
+        |> ignore
+    )
 
-    let certPath =
-        Path.combine
-            (Environment.GetFolderPath(Environment.SpecialFolder.UserProfile))
-            ".office-addin-dev-certs/ca.crt"
+Target.create
+    "CreateDevCerts"
+    (fun _ ->
+        run npx [| "office-addin-dev-certs"; "install"; "--days"; "365" |] ""
+
+        let certPath =
+            Path.combine
+                (Environment.GetFolderPath(Environment.SpecialFolder.UserProfile))
+                ".office-addin-dev-certs/ca.crt"
 
 
-    let psi =
-        new System.Diagnostics.ProcessStartInfo(FileName = certPath, UseShellExecute = true)
+        let psi =
+            new System.Diagnostics.ProcessStartInfo(FileName = certPath, UseShellExecute = true)
 
-    System.Diagnostics.Process.Start(psi) |> ignore)
+        System.Diagnostics.Process.Start(psi) |> ignore
+    )
 
-Target.create "Clean" (fun _ ->
-    Shell.cleanDir deployPath
-    run dotnet [ "fable"; "clean"; "--yes" ] clientPath // Delete *.fs.js files created by Fable
-)
+Target.create
+    "Clean"
+    (fun _ ->
+        Shell.cleanDir deployPath
+        run dotnet [ "fable"; "clean"; "--yes" ] clientPath // Delete *.fs.js files created by Fable
+    )
 
 Target.create "InstallClient" (fun _ -> run npm [ "install" ] ".")
 
@@ -589,6 +622,10 @@ let main args =
         | _ -> runOrDefault args
     | "version" :: a ->
         match a with
+        | "create-file" :: "from-git" :: a ->
+            let version = Release.GetLatestGitTag()
+            ReleaseNoteTasks.createVersionFile (version, true)
+            0
         | "create-file" :: version :: a ->
             ReleaseNoteTasks.createVersionFile (version, false)
             0
