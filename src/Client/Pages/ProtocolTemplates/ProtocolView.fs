e5c7e4df7b7db91e980530db1c17c39d24dfe6aa
patrick blume <PatrickBlume@gmx.de>
2025-08-01 12:44:41 +0200

    Create basic dropdown filter for community selection

--- src/Client/Pages/ProtocolTemplates/ProtocolView.fs
+++ src/Client/Pages/ProtocolTemplates/ProtocolView.fs
@@ -5,10 +5,7 @@ open System
 open Fable
 open Fable.React
 open Fable.React.Props
-//open Fable.Core.JS
 open Fable.Core.JsInterop
-
-//open ISADotNet
 
 open Model
 open Messages
@@ -19,17 +16,65 @@ open Elmish
 
 open Feliz
 open Feliz.DaisyUI
-open ARCtrl
 
-open FileImport
+open Swate.Components
 
 type Templates =
 
+    static member ToggleOrganisation (selectedOrganisations:Set<Model.Protocol.CommunityFilter>, organisation: Model.Protocol.CommunityFilter) =
+        if Set.contains organisation selectedOrganisations then
+            Set.remove organisation selectedOrganisations
+        else
+            Set.add organisation selectedOrganisations
+
+    static member CheckBoxOrganisations
+        (organisations: Array, organisationIndex, isActive: bool, selectedOrganisations, setSelectedOrganisations) =
+
+        let isChecked = selectedOrganisations |> Set.contains organisationIndex
+
+        Html.div [
+            prop.className "swt:flex swt:justify-center"
+            prop.children [
+                Html.input [
+                    prop.type'.checkbox
+                    prop.className "swt:checkbox swt:checkbox-neutral"
+                    prop.disabled (not isActive)
+                    prop.isChecked isChecked
+                    prop.onChange (fun (_: bool) ->
+                        if organisations.Length > 0 then
+                            let nextImportConfig = Templates.ToggleOrganisation(selectedOrganisations, organisationIndex)
+                            setSelectedOrganisations nextImportConfig)
+                ]
+            ]
+        ]
+
     [<ReactComponent>]
     static member Main(model: Model, dispatch) =
+
+        let isOpen, setIsOpen = React.useState (false)
+        let selectedOrganisations, setSelectedOrganisations = React.useState(Set.empty<Model.Protocol.CommunityFilter>)
+
+        let communityNames =
+            model.ProtocolState.Templates
+            |> Array.choose (fun t -> Model.Protocol.CommunityFilter.CommunityFromOrganisation t.Organisation)
+            |> Array.distinct
+            |> List.ofArray
+
+        let communities =
+            [
+                Model.Protocol.CommunityFilter.All
+                Model.Protocol.CommunityFilter.OnlyCurated
+            ]
+            @ communityNames
+            |> Array.ofList
+
+        communities
+        |> Array.iter (fun item ->
+            printfn "options: %s" (item.ToString())
+        )
+
         SidebarComponents.SidebarLayout.Container [
             SidebarComponents.SidebarLayout.Header "Templates"
-
             SidebarComponents.SidebarLayout.Description(
                 Html.p [
                     Html.b "Search the database for templates."
@@ -42,10 +87,35 @@ type Templates =
             )
 
             SidebarComponents.SidebarLayout.LogicContainer [
+                Html.div [
+                    Html.p "Select community"
+
+                    Components.BaseDropdown.Main(
+                        isOpen,
+                        setIsOpen,
+                        Html.button [
+                            prop.onClick (fun _ -> setIsOpen (not isOpen))
+                            prop.role "button"
+                            prop.type'.button
+                            prop.className "swt:btn swt:btn-primary swt:border swt:!border-base-content swt:join-item swt:flex-nowrap"
+                            prop.children [ Icons.Filter() ]
+                        ],
+
+                        communities
+                        |> Array.map (fun community ->
+                            Html.label [
+                                prop.className "swt:flex swt:items-center swt:gap-x-2 swt:cursor-pointer"
+                                prop.children[
+                                    Templates.CheckBoxOrganisations(communities, community, true, selectedOrganisations, setSelectedOrganisations)
+                                    Html.text (community.ToStringRdb())
+                                ]
+                            ]
+                        )
+                    )
+                ]
                 if model.ProtocolState.ShowSearch then
                     Protocol.SearchContainer.Main(model, dispatch)
                 else
                     Modals.SelectiveTemplateFromDB.Main(model, dispatch, false)
             ]
-
         ]
