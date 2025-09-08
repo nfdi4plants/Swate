d58aa341accbea577966b73028d60046af4827c8
Kevin F <Freymaurer@gmx.de>
2025-09-08 13:58:44 +0200

    update components index.js

--- src/Components/src/index.js
+++ src/Components/src/index.js
@@ -1,7 +1,8 @@
 export { default as TermSearch } from './output/TermSearch/TermSearch.fs.ts';
 export { default as TemplateFilter } from './output/Template/TemplateFilter.fs.ts';
 export * as Icons from './output/GenericComponents/Icons.fs.ts';
-export * as ThemeProvider from './output/ThemeProvider/ThemeProvider.fs.ts';
+export { default as ThemeProvider } from './output/ThemeProvider/ThemeProvider.fs.ts';
 export { default as AnnotationTable } from './output/AnnotationTable/AnnotationTable.fs.ts';
+export { default as DataMapTable } from './output/DataMapTable/DataMapTable.fs.ts';
 export { SwateApi, TIBApi } from './output/Util/Api.fs.ts'
 import '../tailwind.css'
