72c5fb61d3be8f42f9448c24463a8f4a396f60fe
Etschbeijer <PatrickBlume@gmx.de>
2026-02-26 09:09:57 +0100

    remove unnecessary sync call

--- src/Electron/src/Renderer/components/FileExplorer.fs
+++ src/Electron/src/Renderer/components/FileExplorer.fs
@@ -77,28 +77,6 @@ let createFileTree (parent: FileItemDTO option) selectedTreeItemPath setSelected
                         setPreviewData (Some data)
                         setPreviewError None
                         setDidSelectFile true
-
-                        let fileType: SaveArcFileRequest option =
-                            match data with
-                            | PreviewData.ArcFileData (fileType, json)->
-                                Some {
-                                    FileType = fileType
-                                    Json = json
-                                }
-                            | _ -> None
-
-                        if fileType.IsSome then
-                            let! result = Api.syncARC fileType.Value
-                            match result with
-                            | Ok () -> ()
-                            | Error exn -> console.log ($"[Renderer] Error: {exn.Message}")
-
-
-                        //if selectedTreeItemPath.IsSome && (selectedTreeItemPath.Value <> item.Path.Value) then
-                        //    match data with
-                        //    | PreviewData.ArcFileData (_, json) -> console.log($"json: {json}")
-                        //    | _ -> console.log($"Nope")
-
                     | Error exn ->
                         console.log ($"[Renderer] Error: {exn.Message}")
                         setPreviewData (None)
