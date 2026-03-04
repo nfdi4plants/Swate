be21980c0d340ab14e7c063bdf2ce23ff24faf2b
caroott <caroline.ott1994@gmail.com>
2026-03-04 15:50:44 +0100

    remove git api workarounds

--- src/Electron/src/Swate.Electron.Shared/Api.fs
+++ src/Electron/src/Swate.Electron.Shared/Api.fs
@@ -50,39 +50,3 @@ let writeFile (request: WriteFileRequest) : JS.Promise<Result<unit, exn>> =
 
 let syncARC (request: SaveArcFileRequest) : JS.Promise<Result<unit, exn>> =
     emitJsExpr (arcVaultApi, request) "$0.syncARC($1)"
-
-let getGitStatus () : JS.Promise<Result<GitStatusDto, exn>> =
-    emitJsExpr arcVaultApi "$0.getGitStatus()"
-
-let getGitDiffSummary () : JS.Promise<Result<GitDiffSummaryDto, exn>> =
-    emitJsExpr arcVaultApi "$0.getGitDiffSummary()"
-
-let gitFetch (request: GitRemoteOperationRequest) : JS.Promise<Result<GitOperationResult, exn>> =
-    emitJsExpr (arcVaultApi, request) "$0.gitFetch($1)"
-
-let gitPull (request: GitRemoteOperationRequest) : JS.Promise<Result<GitOperationResult, exn>> =
-    emitJsExpr (arcVaultApi, request) "$0.gitPull($1)"
-
-let gitPush (request: GitRemoteOperationRequest) : JS.Promise<Result<GitOperationResult, exn>> =
-    emitJsExpr (arcVaultApi, request) "$0.gitPush($1)"
-
-let gitInitRepository (targetPath: string) : JS.Promise<Result<GitOperationResult, exn>> =
-    emitJsExpr (arcVaultApi, targetPath) "$0.gitInitRepository($1)"
-
-let gitCloneRepository (request: GitCloneRepositoryRequest) : JS.Promise<Result<GitOperationResult, exn>> =
-    emitJsExpr (arcVaultApi, request) "$0.gitCloneRepository($1)"
-
-let gitStagePaths (request: GitPathspecRequest) : JS.Promise<Result<GitOperationResult, exn>> =
-    emitJsExpr (arcVaultApi, request) "$0.gitStagePaths($1)"
-
-let gitUnstagePaths (request: GitPathspecRequest) : JS.Promise<Result<GitOperationResult, exn>> =
-    emitJsExpr (arcVaultApi, request) "$0.gitUnstagePaths($1)"
-
-let gitCommit (request: GitCommitRequest) : JS.Promise<Result<GitOperationResult, exn>> =
-    emitJsExpr (arcVaultApi, request) "$0.gitCommit($1)"
-
-let createBranch (request: GitCreateBranchRequest) : JS.Promise<Result<GitOperationResult, exn>> =
-    emitJsExpr (arcVaultApi, request) "$0.createBranch($1)"
-
-let checkoutBranch (request: GitCheckoutBranchRequest) : JS.Promise<Result<GitOperationResult, exn>> =
-    emitJsExpr (arcVaultApi, request) "$0.checkoutBranch($1)"
