969f62bcd8e16cdc8647c8cafdf17e77c85f7b81
Caroline Ott <caroline.ott1994@gmail.com>
2026-04-07 14:33:02 +0200

    restore arctarget/inittarget merge

--- src/Electron/src/Renderer/Components/MainContent/Main.fs
+++ src/Electron/src/Renderer/Components/MainContent/Main.fs
@@ -4,16 +4,54 @@ open Feliz
 open Swate.Electron.Shared
 open Renderer.Components.MainContent.Types
 open Renderer.Types
+open Renderer.Components.MainContent.ArcFilePreviewTarget
+open Renderer.Components.MainContent.TextPreviewTarget
+open Renderer.Components.MainContent.UnknownPreviewTarget
+open Renderer.Components.MainContent.ErrorPreviewTarget
+open Renderer.Components.MainContent.LandingDraftTarget
+open Renderer.Components.MainContent.NotesDraftTarget
+open Renderer.Components.MainContent.NotesSearchTarget
+open Renderer.Components.MainContent.EmptySelectionTarget
+open Renderer.Components.MainContent.GitDiffTarget
+open Renderer.Components.MainContent.GitMergeConflictTarget
+open Renderer.Components.MainContent.GitUnsupportedTarget
 open Renderer.Components.MainContent.DataHubBrowserTarget
-open Renderer.Components.MainContent.InitTarget
-open Renderer.Components.MainContent.ArcTarget
 
 /// This can be further reduced by using the actual contexts instead of passing down the states and setters as props, but this is good enough for now
 [<ReactComponent>]
 let Main (appRootPath: ArcRootPath, pageState: PageState option) =
-    match pageState with
-    | Some PageState.DataHubBrowser -> DataHubBrowserTarget()
-    | _ ->
-        match appRootPath with
-        | None -> InitTarget()
-        | Some _ -> ArcTarget()
+    Html.div [
+        prop.className "swt:size-full swt:flex swt:justify-center"
+        prop.children [
+            match appRootPath, pageState with
+            | _, Some PageState.DataHubBrowser ->
+                DataHubBrowserTarget()
+            | None, _ ->
+                Html.div [
+                    prop.className "swt:flex-1 swt:flex swt:justify-center swt:items-center"
+                    prop.children [ Renderer.Components.InitState.InitState() ]
+                ]
+            | Some _, Some(PageState.ArcFilePage arcFile) ->
+                ArcFilePreviewTarget arcFile
+            | Some _, Some(PageState.TextPage content) ->
+                TextPreviewTarget content
+            | Some _, Some PageState.UnknownPage ->
+                UnknownPreviewTarget()
+            | Some _, Some(PageState.ErrorPage errMsg) ->
+                ErrorPreviewTarget errMsg
+            | Some _, Some PageState.LandingDraftPage ->
+                LandingDraftTarget()
+            | Some _, Some PageState.NotesDraftPage ->
+                NotesDraftTarget()
+            | Some _, Some PageState.NotesSearchPage ->
+                NotesSearchTarget()
+            | Some _, Some(PageState.GitDiffPage diffData) ->
+                GitDiffTarget.Main diffData
+            | Some _, Some(PageState.GitMergeConflictPage mergeData) ->
+                GitMergeConflictTarget.Main mergeData
+            | Some _, Some(PageState.GitUnsupportedPage unsupportedPage) ->
+                GitUnsupportedTarget.Main unsupportedPage
+            | Some _, None ->
+                EmptySelectionTarget()
+        ]
+    ]
