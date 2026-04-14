495a2fcd4ac7cb60cd9bab01d1314c157ef2c1da
caroott <caroline.ott1994@gmail.com>
2026-04-14 16:08:05 +0200

    add popover context to ReactContexts.fs

--- src/Components/src/Util/ReactContexts.fs
+++ src/Components/src/Util/ReactContexts.fs
@@ -92,3 +92,27 @@ module BaseModal =
     }
 
     let BaseModalCtx = React.createContext<BaseModalContext option> (None)
+
+[<Erase; Mangle(false)>]
+module Popover =
+    type PopoverContext = {
+        isOpen: bool
+        setIsOpen: bool -> unit
+        floating: FloatingUI.UseFloatingReturn
+        interactions: FloatingUI.UseInteractionsReturn
+        modal: bool
+        labelId: string
+        descriptionId: string
+        debug: string option
+        portalId: string option
+        preserveTabOrder: bool
+        initialFocus: obj option
+        returnFocus: obj option
+        visuallyHiddenDismiss: obj option
+        closeOnFocusOut: bool option
+    }
+
+    let PopoverCtx = React.createContext<PopoverContext option> None
+
+    [<Hook>]
+    let usePopoverCtx () = React.useContext PopoverCtx
