e3d738bb196b558f953562e53300dc8d8483ed50
Gitarackur <peterosah5@gmail.com>
2026-04-14 13:28:28 +0200

    update: navbar in arcfile editor

--- src/Electron/src/Renderer/Components/MainContent/ArcFilePreviewTarget.fs
+++ src/Electron/src/Renderer/Components/MainContent/ArcFilePreviewTarget.fs
@@ -2,7 +2,6 @@ module Renderer.Components.MainContent.ArcFilePreviewTarget
 
 open Feliz
 open Renderer.Components.MainContent.Helper
-open Renderer.Components.MainElement
 open Renderer.Components.WidgetRegistry
 open Swate.Components.ArcFileEditor
 open Swate.Components
@@ -40,7 +39,13 @@ let ArcFilePreviewTarget (arcFile: ArcFiles) =
             }
             |> Promise.start
 
-    let renderHeader editorState =
-        CreateARCitectNavbar editorState setArcFile onSaveArcFile
+    let renderHeader _ =
+        QuickAccessButton.QuickAccessButton("Save", Icons.Save(), onSaveArcFile)
 
-    Main.ArcFileEditor(arcFile, setArcFile, templateServices, ?navbar = Some renderHeader)
+    Main.ArcFileEditor(
+        arcFile,
+        setArcFile,
+        templateServices,
+        ?header = Some renderHeader,
+        ?widgetServices = Some arcFileEditorWidgetServices
+    )
