42e84714aaac47f855dff5b7b4b6b7460a887330
Etschbeijer <PatrickBlume@gmx.de>
2026-05-07 11:10:57 +0200

    remove unused openings

--- src/Electron/src/Renderer/Components/MainContent/NotesDraftTarget.fs
+++ src/Electron/src/Renderer/Components/MainContent/NotesDraftTarget.fs
@@ -1,14 +1,11 @@
 module Renderer.Components.MainContent.NotesDraftTarget
 
 open Feliz
-open Swate.Components.Landing
 open Swate.Components.Notes.Editor
 open Swate.Components.Shared
-open Swate.Electron.Shared
 open Swate.Electron.Shared.FileIOTypes
 open Swate.Electron.Shared.FileIOHelper
 open ARCtrl.Contract
-open Renderer.Components.MainContent.Types
 
 [<ReactComponent>]
 let NotesDraftTarget () =
