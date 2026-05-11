d10c8e8ed6ffc2e725e3b34a9cf0d1e4b6de54df
Kevin F <Freymaurer@gmx.de>
2026-05-11 15:28:33 +0200

    Restructure file picker widget components

--- src/Components/src/Widgets/FilePickerWidget.fs
+++ src/Components/src/Widgets/FilePickerWidget.fs
@@ -4,12 +4,16 @@ open ARCtrl
 open Fable.Core
 open Feliz
 open Swate.Components
+open Swate.Components.JsBindings
 open Swate.Components.Shared
 open Swate.Components.AnnotationTable
 open Swate.Components.AnnotationTable.Context
 open Swate.Components.Widgets.Context
 
 module private FilePickerWidgetHelper =
+
+    [<Literal>]
+    let RemoveDropId = "__file_picker_remove_target__"
 
     let appendPickedPaths (setPaths: (string[] -> string[]) -> unit) =
         fun (paths: string[]) ->
@@ -49,6 +53,21 @@ module private FilePickerWidgetHelper =
 
     let removePath (setPaths: (string[] -> string[]) -> unit) =
         fun (index: int) -> setPaths (fun current -> current |> Array.removeAt index)
+
+    let movePathById (setPaths: (string[] -> string[]) -> unit) =
+        fun (activeId: string) (overId: string) ->
+            setPaths (fun current ->
+                let oldIndex = current |> Array.tryFindIndex ((=) activeId)
+                let newIndex = current |> Array.tryFindIndex ((=) overId)
+
+                match oldIndex, newIndex with
+                | Some oldIndex, Some newIndex when oldIndex <> newIndex ->
+                    DndKit.arrayMove (ResizeArray current, oldIndex, newIndex) |> Seq.toArray
+                | _ -> current
+            )
+
+    let removePathById (setPaths: (string[] -> string[]) -> unit) =
+        fun (pathId: string) -> setPaths (fun current -> current |> Array.filter (fun path -> path <> pathId))
 
     let insertPaths
         (arcFile: ArcFiles)
@@ -97,10 +116,34 @@ type FilePickerWidget =
         ]
 
     [<ReactComponent>]
-    static member private TableRow(index: int, path: string, movePath: int -> int -> unit, removePath: int -> unit) =
+    static member private SortableTableRow
+        (index: int, path: string, movePath: int -> int -> unit, removePath: int -> unit)
+        =
+        let sortable = DndKit.useSortable ({| id = path |})
+
+        let style = [
+            style.custom ("transform", DndKit.CSS.Transform.toString sortable.transform)
+            style.custom ("transition", sortable.transition)
+        ]
+
         Html.tr [
-            prop.key $"{index}_{path}"
+            prop.key path
+            prop.id path
+            prop.ref sortable.setNodeRef
+            prop.style style
             prop.children [
+                Html.td [
+                    prop.className "swt:w-10"
+                    prop.children [
+                        Html.button [
+                            prop.className "swt:btn swt:btn-ghost swt:btn-xs swt:cursor-grab"
+                            prop.type'.button
+                            yield! prop.spread sortable.attributes
+                            yield! prop.spread sortable.listeners
+                            prop.children [ Icons.ArrowUpDown() ]
+                        ]
+                    ]
+                ]
                 Html.td [ prop.className "swt:w-12 swt:font-mono"; prop.text index ]
                 Html.td [
                     prop.className "swt:max-w-md swt:truncate"
@@ -115,6 +158,7 @@ type FilePickerWidget =
                             prop.children [
                                 Html.button [
                                     prop.className "swt:btn swt:btn-xs swt:join-item"
+                                    prop.type'.button
                                     prop.onClick (fun _ -> movePath index (index - 1))
                                     prop.children [ Icons.ArrowUp() ]
                                 ]
@@ -140,23 +184,133 @@ type FilePickerWidget =
             ]
         ]
 
+    /// TODO: Virtualize paths
     [<ReactComponent>]
-    static member private Table(paths: string[], setPaths: (string[] -> string[]) -> unit) =
+    static member Table(paths: string[], setPaths: (string[] -> string[]) -> unit) =
 
         let movePath = FilePickerWidgetHelper.movePath setPaths
         let removePath = FilePickerWidgetHelper.removePath setPaths
 
-        Html.table [
-            prop.className "swt:table swt:table-xs swt:table-zebra swt:table-fixed swt:min-w-full"
+        Html.div [
+            prop.className
+                "swt:overflow-y-auto swt:overflow-x-auto swt:max-h-[45vh] swt:border swt:border-base-300 swt:rounded-box"
             prop.children [
-                Html.tbody [
-                    for index in 0 .. paths.Length - 1 do
-                        let path = paths.[index]
+                Html.table [
+                    prop.className "swt:table swt:table-xs swt:table-zebra swt:table-fixed swt:min-w-full"
+                    prop.children [
+                        Html.tbody [
+                            for index in 0 .. paths.Length - 1 do
+                                let path = paths.[index]
 
-                        FilePickerWidget.TableRow(index, path, movePath, removePath)
+                                FilePickerWidget.SortableTableRow(index, path, movePath, removePath)
+                        ]
+                    ]
                 ]
             ]
         ]
+
+    [<ReactComponent>]
+    static member private RemoveDropZone(isDragging: bool) =
+        let droppable =
+            DndKit.useDroppable (
+                {|
+                    id = FilePickerWidgetHelper.RemoveDropId
+                |}
+            )
+
+        Html.button [
+            prop.ref droppable.setNodeRef
+            prop.type'.button
+            prop.disabled (not isDragging)
+            prop.className [ "swt:btn swt:btn-sm swt:btn-error"; "swt:btn-dash" ]
+            prop.children [ Icons.Delete(); Html.span "Drop row here to remove" ]
+        ]
+
+    [<ReactComponent>]
+    static member SortPathsButtons(setPaths: (string[] -> string[]) -> unit) =
+
+        let sortAscending = FilePickerWidgetHelper.sortAscending setPaths
+
+        let sortDescending = FilePickerWidgetHelper.sortDescending setPaths
+
+        Html.div [
+            prop.className "swt:join"
+            prop.children [
+                Html.button [
+                    prop.className "swt:btn swt:btn-sm swt:join-item"
+                    prop.onClick (fun _ -> sortAscending ())
+                    prop.children [ Icons.ArrowDownAZ() ]
+                ]
+                Html.button [
+                    prop.className "swt:btn swt:btn-sm swt:join-item"
+                    prop.onClick (fun _ -> sortDescending ())
+                    prop.children [ Icons.ArrowDownZA() ]
+                ]
+            ]
+        ]
+
+    [<ReactComponent>]
+    static member private PathViewer(paths: string[], setPaths: (string[] -> string[]) -> unit) =
+
+        let movePathById = FilePickerWidgetHelper.movePathById setPaths
+        let removePathById = FilePickerWidgetHelper.removePathById setPaths
+        let isDragging, setIsDragging = React.useState false
+
+        let pointerSensor =
+            DndKit.useSensor (
+                DndKit.PointerSensor,
+                {|
+                    activationConstraint = {| distance = 6 |}
+                |}
+            )
+
+        let sensors = DndKit.useSensors [| pointerSensor |]
+
+        let handleDragStart (_: DndKit.IDndKitEvent) = setIsDragging true
+
+        let handleDragCancel (_: DndKit.IDndKitEvent) = setIsDragging false
+
+        let handleDragEnd (event: DndKit.IDndKitEvent) =
+            setIsDragging false
+
+            let active = event.active
+            let over = event.over
+
+            if isNull over |> not then
+                let activeId = string active.id
+                let overId = string over.id
+
+                if overId = FilePickerWidgetHelper.RemoveDropId then
+                    removePathById activeId
+                elif activeId <> overId then
+                    movePathById activeId overId
+
+        let itemIds = ResizeArray paths
+
+        DndKit.DndContext(
+            sensors = sensors,
+            onDragStart = handleDragStart,
+            onDragCancel = handleDragCancel,
+            onDragEnd = handleDragEnd,
+            collisionDetection = DndKit.pointerWithin,
+            children =
+                DndKit.SortableContext(
+                    items = itemIds,
+                    strategy = DndKit.verticalListSortingStrategy,
+                    children =
+                        React.Fragment [
+                            Html.div [
+                                prop.className "swt:flex swt:flex-row swt:items-center swt:justify-between"
+                                prop.children [
+                                    FilePickerWidget.SortPathsButtons(setPaths)
+                                    FilePickerWidget.RemoveDropZone(isDragging)
+                                ]
+                            ]
+
+                            FilePickerWidget.Table(paths, setPaths)
+                        ]
+                )
+        )
 
     [<ReactComponent>]
     static member private ActionButtons(clearPaths: unit -> unit, insertPaths: unit -> unit, canInsert: bool) =
@@ -173,24 +327,6 @@ type FilePickerWidget =
                     prop.disabled (not canInsert)
                     prop.text "Insert file names"
                     prop.onClick (fun _ -> insertPaths ())
-                ]
-            ]
-        ]
-
-    [<ReactComponent>]
-    static member SortPathsButtons(sortAscending, sortDescending) =
-        Html.div [
-            prop.className "swt:join"
-            prop.children [
-                Html.button [
-                    prop.className "swt:btn swt:btn-sm swt:join-item"
-                    prop.onClick (fun _ -> sortAscending ())
-                    prop.children [ Icons.ArrowDownAZ() ]
-                ]
-                Html.button [
-                    prop.className "swt:btn swt:btn-sm swt:join-item"
-                    prop.onClick (fun _ -> sortDescending ())
-                    prop.children [ Icons.ArrowDownZA() ]
                 ]
             ]
         ]
@@ -254,10 +390,6 @@ type FilePickerWidget =
             }
             |> Promise.start
 
-        let sortAscending = FilePickerWidgetHelper.sortAscending setPaths
-
-        let sortDescending = FilePickerWidgetHelper.sortDescending setPaths
-
         let clearPaths = FilePickerWidgetHelper.clearPaths setPaths
 
         let insertPaths =
@@ -271,13 +403,7 @@ type FilePickerWidget =
                 else if hasPaths then
                     let canInsert = hasActiveTableView && selectedCells.IsSome && hasPaths
 
-                    FilePickerWidget.SortPathsButtons(sortAscending, sortDescending)
-
-                    Html.div [
-                        prop.className
-                            "swt:overflow-y-auto swt:overflow-x-auto swt:max-h-[45vh] swt:border swt:border-base-300 swt:rounded-box"
-                        prop.children [ FilePickerWidget.Table(paths, setPaths) ]
-                    ]
+                    FilePickerWidget.PathViewer(paths, setPaths)
 
                     FilePickerWidget.ActionButtons(clearPaths, insertPaths, canInsert)
                 else
