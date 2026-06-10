5375da460cf051bd1c4d5af19445f5e84a8fb364
Kevin F <Freymaurer@gmx.de>
2026-06-10 08:54:10 +0200

    Try to align name case

--- src/Electron/forge.config.js
+++ src/Electron/forge.config.js
@@ -4,7 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-    executableName: 'Swate',
+    executableName: "swate",
   },
   rebuildConfig: {},
   makers: [
