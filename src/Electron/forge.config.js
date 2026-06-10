087fdfd5a8e253e231f8551849d1b5b2cb3ce4d4
Kevin F <Freymaurer@gmx.de>
2026-06-10 09:33:27 +0200

    update workflows; remove "npm ci", format forge.config

--- src/Electron/forge.config.js
+++ src/Electron/forge.config.js
@@ -4,7 +4,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
-    executableName: "swate",
+    executableName: 'swate',
   },
   rebuildConfig: {},
   makers: [
