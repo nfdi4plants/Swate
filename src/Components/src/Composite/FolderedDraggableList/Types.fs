107e1076eb90772838b8324f1c1ed6fd18bc5bc3
caroott <caroline.ott1994@gmail.com>
2026-06-23 13:53:49 +0200

    add foldered draggable list types
         Tue Jun 23 13:53:49 2026 +0200
    type dnd active data
    add foldered draggable list types
    add foldered draggable list component
    compile foldered draggable list
      src/Components/src/Composite/FolderedDraggableList/Types.fs

---
+++ src/Components/src/Composite/FolderedDraggableList/Types.fs
@@ -0,0 +1,40 @@
+module Swate.Components.Composite.FolderedDraggableList.Types
+
+open Feliz
+
+type FolderedDraggableItem<'payload> = {
+    Id: string
+    Label: string
+    Payload: 'payload
+    Color: string option
+    Badge: string option
+    Tooltip: string option
+    Disabled: bool
+}
+
+type FolderedDraggableFolder<'payload> = {
+    Id: string
+    Name: string
+    Color: string option
+    Items: FolderedDraggableItem<'payload> list
+}
+
+type FolderedDraggableData<'payload> = {
+    FolderId: string
+    FolderName: string
+    FolderColor: string option
+    ItemId: string
+    ItemLabel: string
+    ItemColor: string option
+    EffectiveColor: string option
+    Payload: 'payload
+}
+
+type FolderedDraggableItemRender<'payload> = {
+    Folder: FolderedDraggableFolder<'payload>
+    Item: FolderedDraggableItem<'payload>
+    DragData: FolderedDraggableData<'payload>
+    IsDragging: bool
+}
+
+type FolderedDraggableItemRenderFn<'payload> = FolderedDraggableItemRender<'payload> -> ReactElement
