7cc33a60f2f58f02e2a0c3502a80dc1d3c75ac36
Kevin F <Freymaurer@gmx.de>
2026-07-08 15:46:18 +0200

    remove tab rendering information and instead use custom renderer function

--- src/Components/src/Composite/Workspace/WIP_ModelRefactor.fsx
+++ src/Components/src/Composite/Workspace/WIP_ModelRefactor.fsx
@@ -48,8 +48,6 @@ type PaneId =
 type Tab<'T> = {
     Id: TabId
     Label: string
-    Icon: string option
-    ClassName: string option
     Payload: 'T
 }
 
@@ -637,7 +635,7 @@ let update (onErroCallback: exn -> unit) (model: WorkspaceModel<'T>) (msg: Msg<'
 type CompClass =
     [<ReactComponent>]
     static member MyComponent
-        (renderTab: Tab<'A> -> ReactElement, ?initialTabs: Tab<'A>[], ?initialActiveTabId: string, ?onErrorCallback: exn -> unit)
+        (renderTabContent: Tab<'A> -> ReactElement, renderTab: Tab<'A> -> ReactElement, ?initialTabs: Tab<'A>[], ?initialActiveTabId: string, ?onErrorCallback: exn -> unit)
         =
 
         let onErrorCallback = defaultArg onErrorCallback (fun exn -> Browser.Dom.console.error exn)
