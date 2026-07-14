78b49f80d789e0b52710e83b315bf345f72c17ee
Gitarackur <peterosah5@gmail.com>
2026-07-14 10:41:30 +0200

    update

--- build/Tools/Git.fs
+++ build/Tools/Git.fs
@@ -12,21 +12,8 @@ let getTags () =
     |> Array.map (fun t -> if t.StartsWith("v") then t.Substring 1 else t)
 
 let createTagAndPush (tag: string) =
-    run
-        "git"
-        [
-            "tag"
-            tag
-        ]
-        ""
+    run "git" [ "tag"; tag ] ""
 
-    run
-        "git"
-        [
-            "push"
-            "origin"
-            tag
-        ]
-        ""
+    run "git" [ "push"; "origin"; tag ] ""
 
     printGreenfn "Tag %s created and pushed" tag
