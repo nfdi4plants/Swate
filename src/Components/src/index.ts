c2804d894b81a4476728abe7dcd379bcda95c976
Kevin F <Freymaurer@gmx.de>
2026-08-25 15:37:41 +0200

    fix index.ts

--- src/Components/src/index.ts
+++ src/Components/src/index.ts
@@ -16,7 +16,8 @@ export { default as ErrorModalProvider } from './dist/Primitive/ErrorModal/Provi
 export { default as LoadingSpinner } from './dist/Primitive/LoadingSpinner/LoadingSpinner.fs';
 export { default as Navbar } from './dist/Primitive/Navbar/Navbar.fs';
 export { default as Popover } from './dist/Primitive/Popover/Popover.fs';
-export { default as Select } from './dist/Primitive/Select/Select.fs';
+export { default as MultiSelect } from './dist/Primitive/Select/MultiSelect.fs';
+export { default as SingleSelect } from './dist/Primitive/Select/SingleSelect.fs';
 export * as Icons from './dist/Primitive/Icons.fs';
 
 export { DeleteButton, CircularExitButton, CollapseButton, QuickAccessButton } from './dist/Primitive/Buttons/Buttons.fs';
